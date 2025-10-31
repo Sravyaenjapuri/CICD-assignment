5997cac993c55bbffbfb57462dd18c14ebc1010f
Sravya Enjapuri <sravyaenjapuri123@gmail.com>
2025-10-31 12:34:47 -0500

    triggering the pipeline to test

--- test_app.py
+++ test_app.py
@@ -5,3 +5,5 @@ def test_get_message():
         response = client.get('/api/message')
         assert response.status_code == 200
         assert response.json == {'message': 'Hello from Flask!'}
+        print("hi")
+        print("hello")
